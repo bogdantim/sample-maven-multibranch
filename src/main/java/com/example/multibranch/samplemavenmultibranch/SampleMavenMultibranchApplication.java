b6495d6da9bbabff2f759b8cf4afabd94c1f323a
timo <bandit@local>
2019-08-27 23:14:43 +0300

    first commit

---
+++ src/main/java/com/example/multibranch/samplemavenmultibranch/SampleMavenMultibranchApplication.java
@@ -0,0 +1,13 @@
+package com.example.multibranch.samplemavenmultibranch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleMavenMultibranchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleMavenMultibranchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/multibranch/samplemavenmultibranch/SampleMavenMultibranchApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.multibranch.samplemavenmultibranch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SampleMavenMultibranchApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
